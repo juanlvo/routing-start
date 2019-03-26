2657ceef69511490520cfbc474584806d9316feb
Juan Vivas <juanlvo@gmail.com>
2019-03-26 12:17:51 +0100

    feat: Section 11 Lecture 125 fetching route parameters reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -15,7 +15,16 @@ export class UserComponent implements OnInit {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
-    }
+    };
+    this.route.params.subscribe(function(params: Params) {
+      /* this way to declare is working properly
+      this.user.id = params['id'];
+      this.user.name = params['name'];*/
+      this.user = {
+        id:  params['id'],
+        name: params['name']
+      };
+    });
   }
 
 }
